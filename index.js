c62e479aed568671af837c2df666e040ad3d7e1a
Nathan Rajlich <nathan@tootallnate.net>
2013-05-25 14:21:37 -0700

    intial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+
+/**
+ * Module dependencies.
+ */
+
+var url = require('url');
+var qs = require('querystring');
+
+/**
+ * Module exports.
+ */
+
+module.exports = parse;
+
+/**
+ * Parses a "Spotify URI".
+ *
+ * @param {String} uri
+ * @return {Object} parsed Spotify uri object
+ * @api public
+ */
+
+function parse (uri) {
+  var parts;
+  var parsed = url.parse(uri);
+  var rtn = { uri: uri };
+  if ('spotify:' == parsed.protocol) {
+    parts = uri.split(':');
+    parseParts(parts, rtn);
+  } else if ('embed.spotify.com' == parsed.hostname) {
+    return parse(qs.parse(parsed.query).uri);
+  } else { // http: or https:
+    parts = parsed.pathname.split('/');
+    parseParts(parts, rtn);
+  }
+  return rtn;
+}
+
+function parseParts (parts, obj) {
+  var len = parts.length;
+  if (len >= 3 && 'local' == parts[1]) {
+    // local
+    obj.type = 'local';
+    obj.artist = unencode(parts[2]);
+    obj.album = unencode(parts[3]);
+    obj.track = unencode(parts[4]);
+    obj.seconds = +parts[5];
+  } else if (len >= 5) {
+    // playlist
+    obj.user = unencode(parts[2]);
+    obj.type = parts[3];
+    obj.id = parts[4];
+  } else if (len >= 4 && 'starred' == parts[3]) {
+    // starred
+    obj.type = 'playlist';
+    obj.starred = true;
+    obj.user = unencode(parts[2]);
+  } else if (len >= 3) {
+    // track, artist, album
+    obj.type = parts[1];
+    obj.id = parts[2];
+  }
+}
+
+function unencode (str) {
+  return unescape(str).replace(/\+/g, ' ');
+}
---
+++ test/test.js
@@ -0,0 +1,94 @@
+
+/**
+ * Module dependencies.
+ */
+
+var assert = require('assert');
+var parse = require('../');
+
+describe('HTTP URLs', function () {
+  describe('open.spotify.com', function () {
+    it('should parse "track" URLs', function () {
+      var url = 'http://open.spotify.com/track/10M2REwwztVxgr0szw7UwD';
+      var obj = parse(url);
+      assert.equal('track', obj.type);
+      assert.equal('10M2REwwztVxgr0szw7UwD', obj.id);
+    });
+    it('should parse "playlist" URLs', function () {
+      var url = 'http://open.spotify.com/user/tootallnate/playlist/0Lt5S4hGarhtZmtz7BNTeX';
+      var obj = parse(url);
+      assert.equal('playlist', obj.type);
+      assert.equal('tootallnate', obj.user);
+      assert.equal('0Lt5S4hGarhtZmtz7BNTeX', obj.id);
+    });
+    it('should parse "local" URLs', function () {
+      var url = 'http://open.spotify.com/local/Yasunori+Mitsuda/Chrono+Trigger+OST/A+Shot+of+Crisis/161';
+      var obj = parse(url);
+      assert.equal('local', obj.type);
+      assert.equal('Yasunori Mitsuda', obj.artist);
+      assert.equal('Chrono Trigger OST', obj.album);
+      assert.equal('A Shot of Crisis', obj.track);
+      assert.equal(161, obj.seconds);
+    });
+  });
+  describe('play.spotify.com', function () {
+    it('should parse "track" URLs', function () {
+      var url = 'https://play.spotify.com/track/5W3cjX2J3tjhG8zb6u0qHn';
+      var obj = parse(url);
+      assert.equal('track', obj.type);
+      assert.equal('5W3cjX2J3tjhG8zb6u0qHn', obj.id);
+    });
+  });
+  describe('embed.spotify.com', function () {
+    it('should parse "track" URLs', function () {
+      var url = 'https://embed.spotify.com/?uri=spotify:track:5oscsdDQ0NpjsTgpG4bI8S';
+      var obj = parse(url);
+      assert.equal('track', obj.type);
+      assert.equal('5oscsdDQ0NpjsTgpG4bI8S', obj.id);
+    });
+  });
+});
+
+describe('Spotify URIs', function () {
+  it('should parse "ablum" URIs', function () {
+    var uri = 'spotify:album:4m2880jivSbbyEGAKfITCa';
+    var obj = parse(uri);
+    assert.equal('album', obj.type);
+    assert.equal('4m2880jivSbbyEGAKfITCa', obj.id);
+  });
+  it('should parse "artist" URIs', function () {
+    var uri = 'spotify:artist:4tZwfgrHOc3mvqYlEYSvVi';
+    var obj = parse(uri);
+    assert.equal('artist', obj.type);
+    assert.equal('4tZwfgrHOc3mvqYlEYSvVi', obj.id);
+  });
+  it('should parse "track" URIs', function () {
+    var uri = 'spotify:track:5CMjjywI0eZMixPeqNd75R';
+    var obj = parse(uri);
+    assert.equal('track', obj.type);
+    assert.equal('5CMjjywI0eZMixPeqNd75R', obj.id);
+  });
+  it('should parse "playlist" URIs', function () {
+    var uri = 'spotify:user:daftpunkofficial:playlist:6jP6EcvAwqNksccDkIe6hX';
+    var obj = parse(uri);
+    assert.equal('playlist', obj.type);
+    assert.equal('daftpunkofficial', obj.user);
+    assert.equal('6jP6EcvAwqNksccDkIe6hX', obj.id);
+  });
+  it('should parse "local" track URIs', function () {
+    var uri = 'spotify:local:Yasunori+Mitsuda:Chrono+Trigger+OST:A+Shot+of+Crisis:161';
+    var obj = parse(uri);
+    assert.equal('local', obj.type);
+    assert.equal('Yasunori Mitsuda', obj.artist);
+    assert.equal('Chrono Trigger OST', obj.album);
+    assert.equal('A Shot of Crisis', obj.track);
+    assert.equal(161, obj.seconds);
+  });
+  it('should parse "starred" playlist URIs', function () {
+    var uri = 'spotify:user:tootallnate:starred';
+    var obj = parse(uri);
+    assert.equal('playlist', obj.type);
+    assert.equal('tootallnate', obj.user);
+    assert.equal(true, obj.starred);
+  });
+});
