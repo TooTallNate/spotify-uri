9bdadf6beb55598da26d8df86f8f975d0cff0c92
Nathan Rajlich <nathan@tootallnate.net>
2013-05-28 12:28:10 -0700

    formatOpenURL: make related failing tests pass

--- formatOpenURL.js
+++ formatOpenURL.js
@@ -12,6 +12,12 @@ var parse = require('./parse');
 module.exports = formatOpenURL;
 
 /**
+ * Base URL.
+ */
+
+var base = 'http://open.spotify.com';
+
+/**
  * Format a "parsed Spotify object" back into a Spotify "open.spotify.com" URL
  * string.
  *
@@ -22,5 +28,31 @@ module.exports = formatOpenURL;
 
 function formatOpenURL (parsed) {
   if ('string' == typeof parsed) parsed = parse(parsed);
-  return 'http://open.spotify.com/' + parsed.type + '/' + parsed.id;
+  if (parsed.starred) {
+    // "starred" playlist
+    return base + '/user/' + encode(parsed.user) + '/starred';
+  } else if ('playlist' == parsed.type) {
+    // user "playlist"
+    return base + '/user/' + encode(parsed.user) + '/playlist/' + parsed.id;
+  } else if ('local' == parsed.type) {
+    // "local" file
+    return base + '/local/' + encode(parsed.artist) +
+                        '/' + encode(parsed.album) +
+                        '/' + encode(parsed.track) +
+                        '/' + parsed.seconds;
+  } else {
+    // artist, album, track
+    return base + '/' + parsed.type + '/' + parsed.id;
+  }
+}
+
+/**
+ * URL-encode, also turn ` ` (space) chars into `+` (plus).
+ *
+ * @param {String} str
+ * @api private
+ */
+
+function encode (str) {
+  return escape(str.replace(/ /g, '+'));
 }
