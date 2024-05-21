353be0ba427ddd9db9ffc7ec2fc12f55a411ad5b
alantoa <toacncom@gmail.com>
2024-05-21 21:59:40 +0800

    support react native env

--- src/universal-url.ts
+++ src/universal-url.ts
@@ -1,12 +1,17 @@
-let URLClass: typeof URL
+let URLClass: typeof URL;
 
-if (typeof window !== 'undefined' && typeof window.URL !== 'undefined') {
-  URLClass = window.URL
-} else if (typeof require !== 'undefined') {
-  const { URL } = require('url')
-  URLClass = URL
+if (typeof window !== "undefined" && typeof window.URL !== "undefined") {
+  // for browser environment
+  URLClass = window.URL;
+} else if (typeof global !== "undefined" && typeof global.URL !== "undefined") {
+  // for react native environment
+  URLClass = global.URL;
+} else if (typeof require !== "undefined") {
+  // for Node.js environment
+  const { URL } = require("url");
+  URLClass = URL;
 } else {
-  throw new Error('Unsupported environment');
+  throw new Error("Unsupported environment");
 }
 
-export { URLClass as URL }
+export { URLClass as URL };
